android/codelab-android-databinding

Initial commit for the Data Binding codelab
Change-Id: I3b18955f4d582ad481d273f401928ba3c7f547b3

app/src/main/java/com/example/android/databinding/basicsample/util/BindingAdapters.kt
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.ImageViewCompat
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.example.android.databinding.basicsample.R
+import com.example.android.databinding.basicsample.data.Popularity
+
+object BindingAdapters {
+    /**
+     * A Binding Adapter that is called whenever the value of the attribute `app:popularityIcon`
+     * changes. Receives a popularity level that determines the icon and tint color to use.
+     */
+    @BindingAdapter("app:popularityIcon")
+    @JvmStatic
+    fun popularityIcon(view: ImageView, popularity: Popularity) {
+
+        val color = getAssociatedColor(popularity, view.context)
+
+        ImageViewCompat.setImageTintList(view, ColorStateList.valueOf(color))
+
+        view.setImageDrawable(getDrawablePopularity(popularity, view.context))
+    }
+
+    /**
+     * A Binding Adapter that is called whenever the value of the attribute `android:progressTint`
+     * changes. Depending on the value it determines the color of the progress bar.
+     */
+    @BindingAdapter("app:progressTint")
+    @JvmStatic
+    fun tintPopularity(view: ProgressBar, popularity: Popularity) {
+
+        val color = getAssociatedColor(popularity, view.context)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            view.progressTintList = ColorStateList.valueOf(color)
+        }
+    }
+
+    /**
+     *  Sets the value of the progress bar so that 5 likes will fill it up.
+     *
+     *  Showcases Binding Adapters with multiple attributes. Note that this adapter is called
+     *  whenever any of the attribute changes.
+     */
+    @BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+    @JvmStatic
+    fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+        progressBar.progress = (likes * max / 5).coerceAtMost(max)
+    }
+
+    /**
+     * Unused Binding Adapter to replace the Binding Converter that hides a view if the number
+     * of likes is zero.
+     */
+    @BindingAdapter("app:hideIfZero")
+    @JvmStatic
+    fun hideIfZero(view: View, number: Int) {
+        view.visibility = if (number == 0) View.GONE else View.VISIBLE
+    }
+
+    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+        return when (popularity) {
+            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+                intArrayOf(android.R.attr.colorForeground)
+            ).getColor(0, 0x000000)
+            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+        }
+    }
+
+    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+        return when (popularity) {
+            Popularity.NORMAL -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+            }
+            Popularity.POPULAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+            Popularity.STAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+        }
+    }
+}

app/src/main/java/com/example/android/databinding/basicsample/util/BindingConverters.kt
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.databinding.BindingConversion
+import android.view.View
+
+/**
+ * In order to show a View only when it has more than 0 likes, we pass this expression to its
+ * visibilty property:
+ *
+ * `android:visibility="@{ConverterUtil.isZero(viewmodel.likes)}"`
+ *
+ * This converts "likes" (an Int) into a Boolean. See [BindingConverters] for the conversion
+ * from Boolean to a visibility integer.
+ */
+object ConverterUtil {
+    @JvmStatic
+    fun isZero(number: Int): Boolean = number == 0
+}
+
+/**
+ * The number of likes is an integer and the visibility attribute takes an integer
+ * (VISIBLE, GONE and INVISIBLE are 0, 4 and 8 respectively), so we use this converter.
+ *
+ * There is no need to specify that this converter should be used. [BindingConversion]s are
+ * applied automatically.
+ */
+object BindingConverters {
+
+    @BindingConversion
+    @JvmStatic
+    fun booleanToVisibility(isNotVisible: Boolean): Int {
+        return if (isNotVisible) View.GONE else View.VISIBLE
+    }
+}

app/src/main/java/com/example/android/databinding/basicsample/util/BindingMethods.kt
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.databinding.BindingMethod
+import android.databinding.BindingMethods
+import android.widget.ImageView
+
+/**
+ * `app:srcCompat` is an attribute used by the support library to integrate vector drawables. This
+ * BindingMethod binds the attribute to the setImageDrawable method in the ImageView class.
+ *
+ * Binding methods have to be applied to any class in your project. Even an empty one.
+ *
+ * This is equivalent to:
+ * ```
+ *
+ *   @BindingAdapter("app:srcCompat")
+ *   @JvmStatic fun srcCompat(view: ImageView, @DrawableRes drawableId: Int) {
+ *       view.setImageResource(drawable)
+ *   }
+ * ```
+ */
+@BindingMethods(
+    BindingMethod(
+        type = ImageView::class,
+        attribute = "app:srcCompat",
+        method = "setImageResource"
+    )
+)
+class MyBindingMethods

edwinperaza/codelab-databinding

app/src/main/java/com/example/android/databinding/basicsample/util/BindingAdapters.kt
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
+import androidx.databinding.BindingAdapter
+import com.example.android.databinding.basicsample.R
+import com.example.android.databinding.basicsample.data.Popularity
+
+/**
+ * A Binding Adapter that is called whenever the value of the attribute `app:popularityIcon`
+ * changes. Receives a popularity level that determines the icon and tint color to use.
+ */
+@BindingAdapter("app:popularityIcon")
+fun popularityIcon(view: ImageView, popularity: Popularity) {
+
+    val color = getAssociatedColor(popularity, view.context)
+
+    ImageViewCompat.setImageTintList(view, ColorStateList.valueOf(color))
+
+    view.setImageDrawable(getDrawablePopularity(popularity, view.context))
+}
+
+/**
+ * A Binding Adapter that is called whenever the value of the attribute `android:progressTint`
+ * changes. Depending on the value it determines the color of the progress bar.
+ */
+@BindingAdapter("app:progressTint")
+fun tintPopularity(view: ProgressBar, popularity: Popularity) {
+
+    val color = getAssociatedColor(popularity, view.context)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        view.progressTintList = ColorStateList.valueOf(color)
+    }
+}
+
+/**
+ *  Sets the value of the progress bar so that 5 likes will fill it up.
+ *
+ *  Showcases Binding Adapters with multiple attributes. Note that this adapter is called
+ *  whenever any of the attribute changes.
+ */
+@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+    progressBar.progress = (likes * max / 5).coerceAtMost(max)
+}
+
+/**
+ * Unused Binding Adapter to replace the Binding Converter that hides a view if the number
+ * of likes is zero.
+ */
+@BindingAdapter("app:hideIfZero")
+fun hideIfZero(view: View, number: Int) {
+    view.visibility = if (number == 0) View.GONE else View.VISIBLE
+}
+
+private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+    return when (popularity) {
+        Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+            intArrayOf(android.R.attr.colorForeground)
+        ).getColor(0, 0x000000)
+        Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+        Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+    }
+}
+
+private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+    return when (popularity) {
+        Popularity.NORMAL -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+        }
+        Popularity.POPULAR -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+        }
+        Popularity.STAR -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+        }
+    }
+}

app/src/main/java/com/example/android/databinding/basicsample/util/BindingConverters.kt
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.view.View
+import androidx.databinding.BindingConversion
+
+/**
+ * In order to show a View only when it has more than 0 likes, we pass this expression to its
+ * visibilty property:
+ *
+ * `android:visibility="@{ConverterUtil.isZero(viewmodel.likes)}"`
+ *
+ * This converts "likes" (an Int) into a Boolean. See [BindingConverters] for the conversion
+ * from Boolean to a visibility integer.
+ */
+object ConverterUtil {
+    @JvmStatic
+    fun isZero(number: Int): Boolean = number == 0
+}
+
+/**
+ * The number of likes is an integer and the visibility attribute takes an integer
+ * (VISIBLE, GONE and INVISIBLE are 0, 4 and 8 respectively), so we use this converter.
+ *
+ * There is no need to specify that this converter should be used. [BindingConversion]s are
+ * applied automatically.
+ */
+object BindingConverters {
+
+    @BindingConversion
+    @JvmStatic
+    fun booleanToVisibility(isNotVisible: Boolean): Int {
+        return if (isNotVisible) View.GONE else View.VISIBLE
+    }
+}

app/src/main/java/com/example/android/databinding/basicsample/util/BindingMethods.kt
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.util
+
+import android.widget.ImageView
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
+
+/**
+ * `app:srcCompat` is an attribute used by the support library to integrate vector drawables. This
+ * BindingMethod binds the attribute to the setImageDrawable method in the ImageView class.
+ *
+ * Binding methods have to be applied to any class in your project. Even an empty one.
+ *
+ * This is equivalent to:
+ * ```
+ *
+ *   @BindingAdapter("app:srcCompat")
+ *   @JvmStatic fun srcCompat(view: ImageView, @DrawableRes drawableId: Int) {
+ *       view.setImageResource(drawable)
+ *   }
+ * ```
+ */
+@BindingMethods(
+    BindingMethod(
+        type = ImageView::class,
+        attribute = "app:srcCompat",
+        method = "setImageResource"
+    )
+)
+class MyBindingMethods